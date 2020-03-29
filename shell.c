14eb6d31448569df58e47e12102aaabf2448bef0
ceade <chris.eade@gmail.com>
2020-03-30 01:06:51 +0200

    WIP: trying to get the shell have its own buffer
    Basically to emulate a tty line discipline buffer so that things like backspace
    work.

--- shell.c
+++ shell.c
@@ -5,35 +5,82 @@
 
 struct shell* SHP;
 
-uint8_t shell_interpret() {
+uint8_t shell_prompt(struct rb* sob) {
+    rb_write(sob, (uint8_t)'$');
+    return rb_write(sob, (uint8_t)' ');
+}
+
+uint8_t shell_println(struct rb* sob, uint8_t* str) {
+    uint8_t c = *str;
+    while (c) {
+        rb_write(sob, c);
+        str++;
+        c = *str;
+    }
+    rb_write(sob, (uint8_t)'\n');
+    return rb_write(sob, (uint8_t)'\r');
+}
+
+uint8_t shell_interpret(struct rb* sob) {
     // interpret the line buffer contents
     // https://gist.github.com/parse/966049
+    uint8_t idx = 0;
+    uint8_t sidx = 0;
+    for (idx = 0; idx < SHP->write; idx++) {
+        if (SHP->linebuff[idx] == ' ') {
+            SHP->linebuff[idx] = 0;
+            if (sidx == idx) {
+                continue;
+            }
+            shell_println(sob, &SHP->linebuff[sidx]);
+            sidx = idx + 1;
+        }
+    }
+    if (sidx == idx) {
+        return 0;
+    }
+    shell_println(sob, &SHP->linebuff[sidx]);
     return 0;
 }
 
 uint8_t shell_si_consumer(struct rb* buff) {
     uint8_t c;
     struct queue* so = SHP->so;
+    struct rb* sob = &so->buff;
     if (rb_read(buff, &c)) {
         return 1;
     }
-    if (c == '\n') {
-        SHP->linebuff[SHP->idx] = 0;
-        SHP->idx = 0;
-        rb_write(&so->buff, (uint8_t)'\n');
-        rb_write(&so->buff, (uint8_t)'\r');
-        return shell_interpret();
+    if (c == 0x0d) { // r
+        SHP->linebuff[SHP->write] = 0;
+        rb_write(sob, (uint8_t)'\n');
+        rb_write(sob, (uint8_t)'\r');
+        uint8_t ret = shell_interpret(sob);
+        SHP->write = 0;
+        SHP->read = 0;
+        shell_prompt(sob);
+        return ret;
     }
-    if (SHP->idx == 58) {
+    if (c == 0x7f) {
+        SHP->linebuff[SHP->write] = 0;
+        SHP->write--;
+        SHP->read = 0;
+        rb_write(sob, (uint8_t)'\r');
+    }
+    if (SHP->write == SHELL_LINEBUF_LIMIT) {
         return 1;
     }
-    SHP->linebuff[SHP->idx] = c;
-    SHP->idx++;
-    return rb_write(&so->buff, c);
+    SHP->linebuff[SHP->write] = c;
+    SHP->write++;
+    return 0;
 }
 
 uint8_t shell_so_producer(struct rb* buff) {
-    return 0;
+    if (SHP->write == SHP->read) {
+        return 1;
+    }
+    uint8_t ret = rb_write(buff, SHP->linebuff[SHP->read]);
+    SHP->read++;
+    return ret;
 }
 
 uint8_t shell_init() {
@@ -43,7 +90,8 @@ uint8_t shell_init() {
         return 1;
     }
     SHP = sh;
-    SHP->idx = 0;
+    SHP->write = 0;
+    SHP->read = 0;
 
     struct queue* si = (struct queue*)m8_open(0, (uint8_t*)"dev/stdin");
     if (!si) {
@@ -60,6 +108,8 @@ uint8_t shell_init() {
     }
     so->producer = shell_so_producer;
     SHP->so = so;
+
+    shell_prompt(&so->buff);
 
     return 0;
 }
--- shell.h
+++ shell.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
+#define SHELL_LINEBUF_SIZE 58
+#define SHELL_LINEBUF_LIMIT (SHELL_LINEBUF_SIZE - 1)
+
 struct shell {
     struct queue* si;
     struct queue* so;
-    uint8_t idx;
-    uint8_t linebuff[59];
+    uint8_t write;
+    uint8_t read;
+    uint8_t linebuff[SHELL_LINEBUF_SIZE];
 };
 
 uint8_t shell_init();
