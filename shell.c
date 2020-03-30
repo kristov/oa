45884116b9e0ebbba69c677bb51ad82ba216af78
ceade <chris.eade@gmail.com>
2020-03-30 21:35:07 +0200

    Better clarify the purpose of the line buffer
    It is not used for echoing, as it would need to store VT100 escape sequences,
    which would make parsing the line buffer awkward.

--- shell.c
+++ shell.c
@@ -9,11 +9,9 @@
 
 struct shell* SHP;
 
-void shell_prompt() {
-    SHP->linebuff[SHP->write] = '$';
-    SHP->write++;
-    SHP->linebuff[SHP->write] = ' ';
-    SHP->write++;
+void shell_prompt(struct rb* sob) {
+    rb_write(sob, '$');
+    rb_write(sob, ' ');
 }
 
 uint8_t shell_interpret(struct rb* sob) {
@@ -45,34 +43,34 @@ uint8_t shell_si_consumer(struct rb* buff) {
     if (rb_read(buff, &c)) {
         return 1;
     }
+    uint8_t space = rb_space(sob);
     if (c == CHAR_CRETURN) {
+        if (space < 4) {
+            return 1;
+        }
+        rb_write(sob, CHAR_NEWLINE);
+        rb_write(sob, CHAR_CRETURN);
         uint8_t ret = shell_interpret(sob);
+        shell_prompt(sob);
         SHP->write = 0;
-        SHP->read = 0;
-        SHP->linebuff[SHP->write] = CHAR_NEWLINE;
-        SHP->write++;
-        SHP->linebuff[SHP->write] = CHAR_CRETURN;
-        SHP->write++;
-        SHP->linebuff[SHP->write] = 0;
-        SHP->write++;
         return ret;
     }
     if (c == CHAR_BSPACE) {
         if (SHP->write == 0) {
             return 1;
         }
-        SHP->linebuff[SHP->write] = 0;
-        if (SHP->write == SHP->read) {
-            SHP->read--;
+        if (space < 7) {
+            return 1;
         }
+        SHP->linebuff[SHP->write] = 0;
         SHP->write--;
-        rb_write(&so->buff, 0x1b);
-        rb_write(&so->buff, 0x5b);
-        rb_write(&so->buff, 0x44);
-        rb_write(&so->buff, 0x20);
-        rb_write(&so->buff, 0x1b);
-        rb_write(&so->buff, 0x5b);
-        rb_write(&so->buff, 0x44);
+        rb_write(sob, 0x1b);
+        rb_write(sob, 0x5b);
+        rb_write(sob, 0x44);
+        rb_write(sob, 0x20);
+        rb_write(sob, 0x1b);
+        rb_write(sob, 0x5b);
+        rb_write(sob, 0x44);
         return 0;
     }
     if (SHP->write == SHELL_LINEBUF_LIMIT) {
@@ -80,18 +78,10 @@ uint8_t shell_si_consumer(struct rb* buff) {
     }
     SHP->linebuff[SHP->write] = c;
     SHP->write++;
-    return 0;
+    return rb_write(sob, c);
 }
 
 uint8_t shell_so_producer(struct rb* buff) {
-    if (SHP->write == SHP->read) {
-        return 0;
-    }
-    uint8_t ret = rb_write(buff, SHP->linebuff[SHP->read]);
-    if (ret) {
-        return 1;
-    }
-    SHP->read++;
     return 0;
 }
 
